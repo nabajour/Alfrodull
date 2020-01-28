3f0d93a3699c99d98f39739436d9a2a2f26df716
nab <nab@lampshade.ch>
2020-01-28 11:34:36 +0100

    linking to HDF5

--- src/inc/opacities.h
+++ src/inc/opacities.h
@@ -4,43 +4,61 @@
 // initialise variables for ref table management
 // prepares GPU data
 // loads data to GPU
-// interpolates data 
-class OpacityTable {
+// interpolates data
+class OpacityTable
+{
 public:
-  OpacityTable();
+    OpacityTable();
 
-  bool load_opacity_table(const string& filename);
-  bool copy_opacity_table_to_device();
-  
-    
+
 private:
+    string opacity_filename;
 
-  // TODO: read directly and push to device ?
-  std::unique_ptr<H5File> file;
+    bool load_opacity_table(const string& filename);
+    bool copy_opacity_table_to_device();
 
-  // HDF5 tables
-  std::unique_ptr<double> opac_kpoints;
-  
-  std::unique_ptr<double> opac_temperatures;
-  int n_temp = 0;
-  std::unique_ptr<double> opac_pressures;
-  int n_pressures = 0;
 
-  // wieghted Rayeigh c
-  std::unique_ptr<double> scat_cross_sections;
+    // TODO: read directly and push to device ?
+    std::unique_ptr<H5File> file;
 
-  // Mean molecular mass
-  // TODO: needs to be in AMU
-  std::unique_ptr<double> mean_mol_mass;
+    // HDF5 tables
+    std::unique_ptr<double> opac_kpoints;
 
-  std::unique_ptr<double> opac_wave;
-  int nbin = 0;
+    std::unique_ptr<double> opac_temperatures;
+    int                     n_temp = 0;
+    std::unique_ptr<double> opac_pressures;
+    int                     n_pressures = 0;
 
-  std::unique_ptr<double> opac_y;
-  int ny;
+    // wieghted Rayeigh c
+    std::unique_ptr<double> scat_cross_sections;
 
-  std::unique_ptr<double> opac_interwave;
+    // Mean molecular mass
+    // TODO: needs to be in AMU
+    std::unique_ptr<double> mean_mol_mass;
 
-  std::unique_ptr<double> opac_deltawave;
-  
+    std::unique_ptr<double> opac_wave;
+    int                     nbin = 0;
+
+    std::unique_ptr<double> opac_y;
+    int                     ny;
+
+    std::unique_ptr<double> opac_interwave;
+
+    std::unique_ptr<double> opac_deltawave;
+
+    // needed for interpolate_opacities
+    // dev_T_lay
+    // dev_ktemp
+    // dev_p_lay,
+    // 					      dev_kpress,
+    // 						    dev_opac_k,
+    // 						    dev_opac_wg_lay,
+    // 						    dev_opac_scat_cross,
+    // 						    dev_scat_cross_lay,
+    // 						    npress,
+    // 						    ntemp,
+    // 						    ny,
+    // 						    nbin,
+    // 						    fake_opac,
+    // 						    nlayer
 };
