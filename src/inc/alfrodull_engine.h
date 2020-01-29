904af89aa76855520fe4c14087e30a2ba99a93bb
nab <nab@lampshade.ch>
2020-01-29 16:35:09 +0100

    missed file

--- src/inc/alfrodull_engine.h
+++ src/inc/alfrodull_engine.h
@@ -1,4 +1,5 @@
 #include "opacities.h"
+#include "cudaDeviceMemory.h"
 
 class alfrodull_engine
 {
@@ -8,8 +9,30 @@ public:
   void init();
   
   void load_opacities(const string & filename);
+
+  void set_parameters(const int & nlayer_,
+		      const bool & iso_);
+
+  void allocate_internal_variables();
+
+  // TODO: temporary prototyping wrapper for HELIOS. 
+  void get_device_pointers_for_helios_write(double *& dev_scat_cross_section_lay,
+					    double *& dev_scat_cross_section_int,
+					    double *& dev_interwave,
+					    double *& dev_deltawave);
   
   //private:
   opacity_table opacities;
+
+  // general sim parameters
+  int nbin = 0; // should come from opacity table (?)
+
+  int nlayer = 0;
+  int ninterface = 0; // nlayers + 1
+  bool iso = false;
+  
+  // device memory
+  cuda_device_memory<double> scatter_cross_section_lay;
+  cuda_device_memory<double> scatter_cross_section_inter;
   
 };
