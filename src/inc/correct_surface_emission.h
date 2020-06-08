e0aa342537f4ca442db162f69da4538848db4c1b
nab <nab@halo.lampshade.ch>
2020-06-08 10:59:57 +0200

    remove surface emission correction function that's unused

--- src/inc/correct_surface_emission.h
+++
@@ -1,8 +0,0 @@
-
-// adjust the surface emission to satisfy the Stefan-Boltzmann law for the surface temperature
-__global__ void correct_surface_emission(double* delta_lambda,
-                                         double* planckband_lay,
-                                         double  surf_albedo,
-                                         double  T_surf,
-                                         int     nwave,
-                                         int     numlayers);
